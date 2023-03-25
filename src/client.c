d30ebfd04929c6a1688dd2c0c2e3dfee37fac6cd
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-25 09:17:16 +0100

    feat: client now sends string and waits for signal back; server sends signal

--- src/client.c
+++ src/client.c
@@ -6,21 +6,53 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 20:56:42 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/24 22:41:12 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/25 09:08:56 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	ft_handle_sigusr(int sig)
+{
+	(void)sig;
+	return ;
+}
+
+void	ft_dec_to_bin(char c, pid_t server_pid)
+{
+	int i;
+
+	i = 8;
+	while (i--)
+	{
+		if (c % 2 == 0)
+			kill(server_pid, SIGUSR1);
+		else
+			kill(server_pid, SIGUSR2);
+		c /= 2;
+		pause();
+	}
+
+}
+
 int main(int argc, char *argv[])
 {
-	int pid;
+	pid_t				server_pid;
+	struct sigaction	act;
+	char				*input;
 
 	if (argc != 3)
 	{
 		ft_printf("Please enter pid and then a string\n");
 		return (1);
 	}
-	pid = ft_atoi(argv[1]);
-	kill(pid, SIGUSR1);
+	server_pid = ft_atoi(argv[1]);
+	input = argv[2];
+	act.sa_handler = ft_handle_sigusr;
+	sigaction(SIGUSR1, &act, NULL);
+	while (*input != '\0')
+	{
+		ft_dec_to_bin(*input, server_pid);
+		input++;
+	}
 }
--- src/server.c
+++ src/server.c
@@ -6,27 +6,63 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 20:56:49 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/24 22:40:05 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/25 09:10:54 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	ft_handle_sig(int sig)
+void	ft_convert_bin2dec(int sig, bool reset)
 {
+	static uint32_t	byte;
+	static uint8_t	bit_count;
+
+	if (reset)
+	{
+		byte = 0;
+		bit_count = 0;
+		return ;
+	}
 	if (sig == SIGUSR1)
-		ft_printf("received 1\n");
+		byte &= ~(1 << bit_count);
 	else if	(sig == SIGUSR2)
-		ft_printf("received 2\n");
+		byte |= (1 << bit_count);
+	else
+		return;
+	bit_count++;
+	if (bit_count == 8)
+	{
+		write(0, &byte, 1);
+		byte = 0;
+		bit_count = 0;
+	}
+}
+
+void	ft_handle_sigusr(int sig, siginfo_t *info, void *ucontext)
+{
+	static pid_t	last_client;
+
+	(void)ucontext;
+	if (!last_client)
+		last_client = info->si_pid;
+	else if (last_client != info->si_pid)
+	{
+		ft_convert_bin2dec(sig, true);
+		last_client = info->si_pid;
+	}
+	ft_convert_bin2dec(sig, false);
+	kill(info->si_pid, SIGUSR1);
 }
 
 int main(void)
 {
-	struct sigaction	sa;
+	struct sigaction	act;
 
-	ft_printf("This is my pid: %d", getpid());
-	sa.sa_handler = ft_handle_sig;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
-	pause();
+	ft_printf("This is my pid: %d\n", getpid());
+	act.sa_flags = SA_SIGINFO;
+	act.sa_sigaction = ft_handle_sigusr;
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
+	while (1)
+		pause();
 }
