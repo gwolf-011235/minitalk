1e6a718d34acce4b3a76240fb86945f56aca24e4
Gabriel <gwolf@student.42vienna.com>
2023-03-30 14:44:45 +0200

    fix: made main smaller for norminette

--- include/client.h
+++ include/client.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   client.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
+/*   By: gwolf <gwolf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 11:00:11 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/25 11:00:59 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/30 14:43:27 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,10 @@
 # include <signal.h>
 
 # include "ft_print.h"
+
+bool	ft_is_valid_pid(char *pid);
+void	ft_send_to_server(char *input, pid_t server_pid);
+void	ft_convert_dec2bin(uint8_t dec, pid_t server_pid);
+void	ft_handle_sigusr(int sig);
 
 #endif
--- src/client.c
+++ src/client.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
+/*   By: gwolf <gwolf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 20:56:42 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/29 16:09:45 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/30 14:41:38 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
 
-bool	msg;
+bool	g_wait_for_response;
 
 void	ft_handle_sigusr(int sig)
 {
 	(void)sig;
-	msg = false;
+	g_wait_for_response = false;
 	return ;
 }
 
@@ -29,50 +29,63 @@ void	ft_convert_dec2bin(uint8_t dec, pid_t server_pid)
 	i = 0;
 	while (i < 8)
 	{
-		msg = true;
+		g_wait_for_response = true;
 		bit = (dec & (1 << i));
 		if (bit == 0)
 			kill(server_pid, SIGUSR1);
 		else
 			kill(server_pid, SIGUSR2);
 		i++;
-		while (msg)
+		while (g_wait_for_response)
 			;
 	}
+}
+
+void	ft_send_to_server(char *input, pid_t server_pid)
+{
+	while (*input != '\0')
+	{
+		ft_convert_dec2bin(*input, server_pid);
+		input++;
+	}
+}
+
+bool	ft_is_valid_pid(char *pid)
+{
+	uint32_t	i;
+
+	i = 0;
+	while (pid[i])
+	{
+		if (!ft_isdigit(pid[i]))
+		{
+			return (false);
+		}
+		i++;
+	}
+	return (true);
 }
 
 int	main(int argc, char *argv[])
 {
 	pid_t				server_pid;
 	struct sigaction	act;
-	char				*input;
-	uint32_t			i;
 
 	if (argc != 3)
 	{
 		ft_printf("Please enter PID and then a string\n");
 		return (1);
 	}
-	i = 0;
-	while (argv[1][i])
-	{
-		if (!ft_isdigit(argv[1][i]))
-		{
-			ft_printf("Please enter a valid PID\n");
-			return (1);
-		}
-		i++;
-	}
 	server_pid = ft_atoi(argv[1]);
-	input = argv[2];
+	if (!ft_is_valid_pid(argv[1]))
+	{
+		ft_printf("Please enter a valid PID\n");
+		return (1);
+	}
 	act.sa_handler = ft_handle_sigusr;
 	sigemptyset(&act.sa_mask);
 	act.sa_flags = 0;
 	if (sigaction(SIGUSR1, &act, NULL) == -1)
 		exit(1);
-	while (*input != '\0')
-	{
-		ft_convert_dec2bin(*input, server_pid);
-		input++;
-	}
+	ft_send_to_server(argv[2], server_pid);
 }
